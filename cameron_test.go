d3eafd5065014f501e1b9b0fde3aff5335f7a470
Aofei Sheng <aofei@aofeisheng.com>
2018-05-06 06:16:24 +0800

    test: add `TestNewIdenticon()`

---
+++ cameron_test.go
@@ -0,0 +1,23 @@
+package cameron
+
+import (
+	"bytes"
+	"crypto/md5"
+	"fmt"
+	"image/jpeg"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewIdenticon(t *testing.T) {
+	buf := &bytes.Buffer{}
+	jpeg.Encode(buf, NewIdenticon([]byte("cameron"), 10, 1), &jpeg.Options{
+		Quality: jpeg.DefaultQuality,
+	})
+	assert.Equal(
+		t,
+		fmt.Sprintf("%x", md5.Sum(buf.Bytes())),
+		"2d809f43b9ab5e2f80966824a3b6f94b",
+	)
+}
