57c83e8d17a0cedde779a79caeb813e857d540be
Aofei Sheng <aofei@aofeisheng.com>
2021-10-28 16:56:04 +0800

    test: no longer use github.com/stretchr/testify/assert

--- cameron_test.go
+++ cameron_test.go
@@ -5,19 +5,27 @@ import (
 	"testing"
 
 	"github.com/cespare/xxhash/v2"
-	"github.com/stretchr/testify/assert"
 )
 
 func TestIdenticon(t *testing.T) {
 	d := xxhash.New()
 	jpeg.Encode(d, Identicon([]byte("cameron"), 540, 60), nil)
-	assert.Equal(t, uint64(0x88a6d1f1f6986239), d.Sum64())
+	digest := d.Sum64()
+	if want := uint64(0x88a6d1f1f6986239); digest != want {
+		t.Errorf("got %d, want %d", digest, want)
+	}
 
 	d.Reset()
 	jpeg.Encode(d, Identicon([]byte("cameron"), 540, 65), nil)
-	assert.Equal(t, uint64(0x4e18c5d23e0bec77), d.Sum64())
+	digest = d.Sum64()
+	if want := uint64(0x4e18c5d23e0bec77); digest != want {
+		t.Errorf("got %d, want %d", digest, want)
+	}
 
 	d.Reset()
 	jpeg.Encode(d, Identicon([]byte("cameron"), 540, 1080), nil)
-	assert.Equal(t, uint64(0x3ec4415b1f794db7), d.Sum64())
+	digest = d.Sum64()
+	if want := uint64(0x3ec4415b1f794db7); digest != want {
+		t.Errorf("got %d, want %d", digest, want)
+	}
 }
--- go.mod
+++ go.mod
@@ -2,9 +2,4 @@ module github.com/aofei/cameron
 
 go 1.13
 
-require (
-	github.com/cespare/xxhash/v2 v2.1.1
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/stretchr/testify v1.7.0
-	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
-)
+require github.com/cespare/xxhash/v2 v2.1.2
